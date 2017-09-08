e93fa0585155735907da856dece26f108afe3a7a
Jerry Jheng <dlgs5100@gmail.com>
2017-09-08 21:50:32 +0800

    first commit

---
+++ Best travel/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Best travel")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Best travel")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("93bc0a2a-805a-4116-8c64-fac30458b5fe")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Best travel/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using NUnit.Framework;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Best_travel
+{
+    [TestFixture]
+    public class SumOfKTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Console.WriteLine("****** Basic Tests");
+            List<int> ts = new List<int> { 50, 55, 56, 57, 58 };
+            int? n = SumOfK.chooseBestSum(163, 3, ts);
+            Assert.AreEqual(163, n);
+
+            ts = new List<int> { 50 };
+            n = SumOfK.chooseBestSum(163, 3, ts);
+            Assert.AreEqual(null, n);
+
+            ts = new List<int> { 91, 74, 73, 85, 73, 81, 87 };
+            n = SumOfK.chooseBestSum(230, 3, ts);
+            Assert.AreEqual(228, n);
+        }
+    }
+    public static class SumOfK
+    {
+        //Reference by : https://stackoverflow.com/questions/127704/algorithm-to-return-all-combinations-of-k-elements-from-n
+        public static IEnumerable<IEnumerable<T>> Combinations<T>(this IEnumerable<T> elements, int k)
+        {
+            return k == 0 ? new[] { new T[0] } :
+              elements.SelectMany((e, i) =>
+                elements.Skip(i + 1).Combinations(k - 1).Select(c => (new[] { e }).Concat(c)));
+        }
+        public static int? chooseBestSum(int maxDistance, int amountCountry, List<int> ts)
+        {
+            var result = Combinations(ts, amountCountry).ToList();
+            result.RemoveAll(n => n.ToList().Sum() > maxDistance);
+            if (!result.Any())
+                return null;
+            else
+                return result.Select(m => m.Sum()).Max();
+        }
+    }
+}
